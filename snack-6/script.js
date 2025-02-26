0b1791096e0b8836c686587d4b7bb5b399eccb52
“AntonioAmbrosecchia” <antonyambro96@gmail.com>
2025-02-26 16:31:28 +0100

    half of the snacs, completed

--- snack-6/script.js
+++ snack-6/script.js
@@ -12,3 +12,21 @@ const zucchine = [
 ];
 
 // A partire dall'array fornito, crea due array. Uno con le zucchine più lunghe di almeno 15cm. L'altro con le restanti.
+
+const zucchineL = zucchine.filter((element) => {
+  if (element.length > 15) {
+    return true
+  }
+  return false
+});
+
+console.log(zucchineL);
+
+const zucchineS = zucchine.filter((element) => {
+  if (element.length < 15) {
+    return true
+  }
+  return false
+});
+
+console.log(zucchineS);
