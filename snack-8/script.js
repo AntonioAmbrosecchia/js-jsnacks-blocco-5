dca9d9c9224e0e20458188aa2d6f216dec585e8b
“AntonioAmbrosecchia” <antonyambro96@gmail.com>
2025-02-26 16:50:38 +0100

    end snaks

--- snack-8/script.js
+++ snack-8/script.js
@@ -21,3 +21,8 @@ const students = [
 
 // Recupera la classe dello studente 'Marco Lanci'
 // Risultato: '3C'
+
+const classStudents = students.find((student) => {
+  if (student.name === "Marco Lanci")
+    console.log(student.class);
+});
